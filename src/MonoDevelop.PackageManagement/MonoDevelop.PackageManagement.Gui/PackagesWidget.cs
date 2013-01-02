59630e36a61113cab2df2dd9d78696559421006c
Matt Ward <ward.matt@gmail.com>
2013-01-02 16:42:25 +0000

    Populate package sources in Manage Packages dialog.
    Package sources now added to combo box.
    Selected package source in combo box updates the active package source in the NuGet.config file.

--- src/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Gui/PackagesWidget.cs
+++ src/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Gui/PackagesWidget.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+
 using ICSharpCode.PackageManagement;
+using NuGet;
 
 namespace MonoDevelop.PackageManagement
 {
@@ -8,6 +12,7 @@ namespace MonoDevelop.PackageManagement
 	public partial class PackagesWidget : Gtk.Bin
 	{
 		PackagesViewModel viewModel;
+		List<PackageSource> packageSources;
 		
 		public PackagesWidget ()
 		{
@@ -20,19 +25,57 @@ namespace MonoDevelop.PackageManagement
 			
 			this.packageSearchHBox.Visible = viewModel.IsSearchable;
 			PopulatePackageSources ();
+			this.packageSourceComboBox.Changed += PackageSourceChanged;
 			
 			viewModel.PropertyChanged += ViewModelPropertyChanged;
+		}
+		
+		List<PackageSource> PackageSources {
+			get {
+				if (packageSources == null) {
+					packageSources = viewModel.PackageSources.ToList ();
+				}
+				return packageSources;
+			}
 		}
 		
 		void PopulatePackageSources ()
 		{
 			this.packageSourceComboBox.Visible = viewModel.IsSearchable;
 			if (viewModel.IsSearchable) {
+				for (int index = 0; index < PackageSources.Count; ++index) {
+					PackageSource packageSource = PackageSources [index];
+					this.packageSourceComboBox.InsertText (index, packageSource.Name);
+				}
 				
+				this.packageSourceComboBox.Active = GetSelectedPackageSourceIndexFromViewModel ();
 			}
 		}
+		
+		int GetSelectedPackageSourceIndexFromViewModel ()
+		{
+			if (viewModel.SelectedPackageSource == null) {
+				return -1;
+			}
+			
+			return PackageSources.IndexOf (viewModel.SelectedPackageSource);
+		}
+		
+		void PackageSourceChanged (object sender, EventArgs e)
+		{
+			viewModel.SelectedPackageSource = GetSelectedPackageSource ();
+		}
+		
+		PackageSource GetSelectedPackageSource ()
+		{
+			if (this.packageSourceComboBox.Active == -1) {
+				return null;
+			}
+			
+			return PackageSources [this.packageSourceComboBox.Active];
+		}
 
-		void ViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+		void ViewModelPropertyChanged (object sender, PropertyChangedEventArgs e)
 		{
 			this.packagesListTextView.Buffer.Text += "PropertyChanged: " + e.PropertyName + "\r\n";
 		}
