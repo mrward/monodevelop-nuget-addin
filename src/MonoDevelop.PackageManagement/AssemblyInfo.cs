60b3d61adb186354f5f42fa2c36d78dd057c3538
Matt Ward <ward.matt@gmail.com>
2013-02-09 10:30:40 +0000

    Update version to 0.4

--- src/MonoDevelop.PackageManagement/AssemblyInfo.cs
+++ src/MonoDevelop.PackageManagement/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MonoDevelop.PackageManagement")]
-[assembly: AssemblyCopyright("Copyright (C) 2012 Matthew Ward")]
+[assembly: AssemblyCopyright("Copyright (C) 2012-2013 Matthew Ward")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3")]
+[assembly: AssemblyVersion("0.4")]
